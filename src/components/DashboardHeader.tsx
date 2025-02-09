32fe861b5770f77254e69dac08c320122ce3c692
shinobiKoda <sirp2804@gmail.com>
2025-02-09 17:21:56 +0100

    implement header design

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -6,9 +6,9 @@ import HeaderCalender from './HeaderCalender'
 export default function DashboardHeader() {
   return (
     <div className='w-full flex items-center justify-between p-4'>
-      <div>
-        <h1>Dashboard</h1>
-        <p>Information about your current plan and usages</p>
+      <div className='flex flex-col gap-1'>
+        <h1 className='text-[28.16px] font-bold'>Dashboard</h1>
+        <p className='text-[#92959E] text-[12.67px]'>Information about your current plan and usages</p>
       </div>
       <HeaderCalender />
     </div>
