f7fa860f3605cc8e7873d4600955b51e61c16d4b
PraiseCodex <asoriwaowarida@gmail.com>
2025-02-11 07:17:40 +0100

    increase activity font weight

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -12,11 +12,11 @@ export default function Activity() {
         <MoreVerticalIcon />
       </div>
       <div className="flex gap-2 ">
-        <div>
-          <div className="w-12 h-12 rounded-full bg-gray-300"></div>
-          <div>
-            <p>James Anderson</p>
-            <p>
+        <div className="flex gap-2">
+          <div className="min-w-12 h-12 rounded-full bg-gray-300"></div>
+          <div className="flex flex-col gap-2">
+            <h4 className="font-bold">James Anderson</h4>
+            <p className="text-[]">
               Called "Books-Api" with the javascript webhook and commented.
               "Books-API was readily available with proper documentation and
               realiability of a proper API. It was just a webhook away from
